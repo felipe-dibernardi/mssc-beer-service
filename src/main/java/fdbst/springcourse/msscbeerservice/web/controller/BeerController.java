38ab386296f8627d5562947f47ef1825b3e728c6
Felipe Di Bernardi S Thiago <felipe.thiago@supero.com.br>
2019-09-16 07:56:37 -0300

    Added initial BeerController

---
+++ src/main/java/fdbst/springcourse/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package fdbst.springcourse.msscbeerservice.web.controller;
+
+import fdbst.springcourse.msscbeerservice.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Classe BeerController
+ * <p>
+ * Essa classe é responsável por...
+ *
+ * @author Felipe Di Bernardi S Thiago
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("id") UUID id) {
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beer) {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateBeer(@PathVariable("id") UUID id, @RequestBody BeerDTO beer) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
